6b56e0c86902d632eb4e4ff391561219ae7a7212
EmirhanSyl <61618968+EmirhanSyl@users.noreply.github.com>
2023-08-12 00:30:59 +0300

    Network Connection Issue Solved+Server Checker Add

--- Assets/Scripts/AvailableHostHandler.cs
+++ Assets/Scripts/AvailableHostHandler.cs
@@ -1,9 +1,14 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Threading;
 using System.Threading.Tasks;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UI;
 
 public class AvailableHostHandler : MonoBehaviour
@@ -21,28 +26,90 @@ public class AvailableHostHandler : MonoBehaviour
     private CancellationToken cancellationToken;
 
     private TcpClient client = new TcpClient();
+    private System.Net.NetworkInformation.Ping _ping = new System.Net.NetworkInformation.Ping();
+    private PingReply _reply;
 
     void Start()
     {
         cancellationToken = cts.Token;
 
-        refreshServersBtn.onClick.AddListener(RefreshAvailableServersAsync);
+        refreshServersBtn.onClick.AddListener(CheckNetwork);
 
-        if (IsServerReachable("127.0.0.1", port))
-        {
-            var initiatedServer = Instantiate(availableServerTemplate, templateParent);
-            initiatedServer.GetComponentInChildren<TMP_Text>().text = "127.0.0.1";
-
-            initiatedServer.GetComponentInChildren<Button>().onClick.AddListener(() =>
-            {
-                NetworkManagerController.ConnectSpesifiedServer("127.0.0.1", (ushort)port);
-            });
-        }
+        //CheckServerAvailability("192.168.1.200");
     }
 
     void Update()
     {
 
+    }
+
+    private void CheckNetwork()
+    {
+        CheckAvailableServers();
+    }
+    private async Task CheckAvailableServers()
+    {
+        var tasks = new List<Task>();
+
+        // Divide the IP address range into smaller chunks
+        int chunkSize = 16; 
+
+        for (int i = 1; i < 256; i += chunkSize)
+        {
+            List<string> ipAddresses = GenerateIPAddressesInRange(i, i + chunkSize);
+            tasks.Add(Task.Run(async () =>
+            {
+                foreach (string ipAddress in ipAddresses)
+                {
+                    Debug.Log("Iteration: " + ipAddress);
+                    if (await CheckServerAvailabilityAsync(ipAddress))
+                    {
+                        var initiatedServer = Instantiate(availableServerTemplate, templateParent);
+                        initiatedServer.GetComponentInChildren<TMP_Text>().text = ipAddress;
+
+                        initiatedServer.GetComponentInChildren<Button>().onClick.AddListener(() =>
+                        {
+                            NetworkManagerController.ConnectSpesifiedServer(ipAddress, (ushort)port);
+                        });
+                    }
+                }
+            }));
+        }
+
+        await Task.WhenAll(tasks);
+    }
+
+    private List<String> GenerateIPAddressesInRange(int start, int to)
+    {
+        var ipAddresses = new List<string>();
+
+        for (int i = start; i < to; i++)
+        {
+            string newIP = ipAddressPrefix + i;
+            ipAddresses.Add(newIP);
+        }
+        return ipAddresses;
+    }
+
+
+    private async Task<bool> CheckServerAvailabilityAsync(string ip)
+    {
+        try
+        {
+            var replyTask = Task.Run(() => _ping.Send(ip));
+            await Task.WhenAny(replyTask, Task.Delay(200));
+
+            if (replyTask.IsCompleted && replyTask.Result.Status == IPStatus.Success)
+            {
+                return true;
+            }
+
+            return false;
+        }
+        catch
+        {
+            return false;
+        }
     }
 
     private void RefreshAvailableServersAsync()
--- Assets/Scripts/NetworkManagerController.cs
+++ Assets/Scripts/NetworkManagerController.cs
@@ -7,6 +7,9 @@ using TMPro;
 using System.Linq;
 using UnityEngine.SceneManagement;
 using Unity.Netcode.Transports.UTP;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using IronPython.Runtime;
 
 public class NetworkManagerController : MonoBehaviour
 {
@@ -14,6 +17,7 @@ public class NetworkManagerController : MonoBehaviour
     [SerializeField] private Button startServerBtn;
     [SerializeField] private Button startClientBtn;
 
+    [SerializeField] private TMP_Text headerText;
     [SerializeField] private TMP_Text statusText;
     [SerializeField] private TMP_InputField ipField;
 
@@ -22,19 +26,23 @@ public class NetworkManagerController : MonoBehaviour
 
         startHostBtn.onClick.AddListener(() =>
         {
-            SetConnection();
+            SetConnection(GetDeviceIP(), 7777);
             NetworkManager.Singleton.StartHost();
+            var connData = NetworkManager.Singleton.GetComponent<UnityTransport>().ConnectionData;
+            statusText.text = "Server Created. Server ip: " + connData.Address + " Server port: " + connData.Port;
         });
 
         startServerBtn.onClick.AddListener(() =>
         {
-            SetConnection();
+            SetConnection(GetDeviceIP(), 7777);
             NetworkManager.Singleton.StartServer();
+            var connData = NetworkManager.Singleton.GetComponent<UnityTransport>().ConnectionData;
+            statusText.text = "Server Created. Server ip: " + connData.Address + " Server port: " + connData.Port;
         });
 
         startClientBtn.onClick.AddListener(() =>
         {
-            SetConnection();
+            SetConnection(ipField.text, 7777);
             NetworkManager.Singleton.StartClient();
         });
         if (NetworkManager.Singleton != null)
@@ -47,28 +55,24 @@ public class NetworkManagerController : MonoBehaviour
 
     }
 
-    private void Start()
-    {
-        ConnectSpesifiedServer("127.0.0.1", 7777);
-        StartCoroutine(StartPing("192.168.1.200"));
-    }
-
-    IEnumerator StartPing(string ip)
-    {
-        WaitForSeconds f = new WaitForSeconds(0.05f);
-        Ping p = new Ping(ip);
-        while (p.isDone == false)
-        {
-            yield return f;
-        }
-        PingFinished(p);
-    }
+    #region PING
+    //IEnumerator StartPing(string ip)
+    //{
+    //    WaitForSeconds f = new WaitForSeconds(0.05f);
+    //    Ping p = new Ping(ip);
+    //    while (p.isDone == false)
+    //    {
+    //        yield return f;
+    //    }
+    //    PingFinished(p);
+    //}
 
 
-    public void PingFinished(Ping p)
-    {
-        Debug.Log("Ping Done for ip: " + p.ip + " time: " + p.time);
-    }
+    //public void PingFinished(Ping p)
+    //{
+    //    Debug.Log("Ping Done for ip: " + p.ip + " time: " + p.time);
+    //}
+    #endregion
 
     private void Update()
     {
@@ -85,35 +89,62 @@ public class NetworkManagerController : MonoBehaviour
 
     IEnumerator LoadSceneCorutine(int sceneIndex)
     {
-        statusText.text = "Connection established! Launching App...";
+        headerText.text = "Connection established! Launching App...";
         yield return new WaitForSeconds(5f);
         SceneManager.LoadScene(sceneIndex);
     }
 
-    private void SetConnection()
+    private static void SetConnection(string ip, int port)
     {
-        string ip = ipField.text;
         if (!string.IsNullOrEmpty(ip))
         {
             NetworkManager.Singleton.GetComponent<UnityTransport>().SetConnectionData(
                 ip,
-                7777,
+                (ushort)port,
                 "0.0.0.0"
             );
         }
     }
+
 
     public static void ConnectSpesifiedServer(string ip, ushort port)
     {
         if (!string.IsNullOrEmpty(ip))
         {
-            NetworkManager.Singleton.GetComponent<UnityTransport>().SetConnectionData(
-                ip,
-                port,
-                "0.0.0.0"
-            );
+            SetConnection(ip, port);
 
             NetworkManager.Singleton.StartClient();
         }
+    }
+
+    private string GetDeviceIP()
+    {
+        string ipAddress = "";
+
+        // Get all available network interfaces
+        NetworkInterface[] networkInterfaces = NetworkInterface.GetAllNetworkInterfaces();
+
+        foreach (NetworkInterface networkInterface in networkInterfaces)
+        {
+            // Ignore loopback and non-operational interfaces
+            if (networkInterface.OperationalStatus != OperationalStatus.Up || networkInterface.NetworkInterfaceType == NetworkInterfaceType.Loopback)
+                continue;
+
+            // Get IP properties for the network interface
+            IPInterfaceProperties ipProperties = networkInterface.GetIPProperties();
+
+            // Get the IPv4 addresses associated with the network interface
+            foreach (UnicastIPAddressInformation ipInfo in ipProperties.UnicastAddresses)
+            {
+                if (ipInfo.Address.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    ipAddress = ipInfo.Address.ToString();
+                    break;
+                }
+            }
+
+        }
+
+        return ipAddress;
     }
 }
